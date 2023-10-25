857712074030c39d5a3e09aa99c9d47023cc195f
SiemensHalske <hendriksiemens1@gmail.com>
2023-10-25 21:44:38 +0200

    .

--- src/test_torch.py
+++ src/test_torch.py
@@ -47,11 +47,32 @@ train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=4, shuffle=False, num_workers=2)
 
 # Define the loss function and optimizer
+# Define the CNN architecture
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# Define the CNN model
+cnn = CNN()
+
+# Define the loss function and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(cnn.parameters(), lr=0.001, momentum=0.9)
-
-# Train the CNN
-cnn = CNN()
 for epoch in range(2):
     running_loss = 0.0
     for i, data in enumerate(train_loader, 0):
