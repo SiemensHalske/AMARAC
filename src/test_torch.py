66b721a6b71bb7437c703916aa11ef98aff7bced
SiemensHalske <hendriksiemens1@gmail.com>
2023-10-25 21:54:16 +0200

    123

--- src/test_torch.py
+++ src/test_torch.py
@@ -29,43 +29,12 @@ import os
 from torchvision.datasets.utils import download_url
 
 # Define the training and testing datasets
-train_dataset_url = 'http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
-test_dataset_url = 'http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
-
-if not os.path.exists('./data'):
-    os.makedirs('./data')
-
-if not os.path.exists('./data/cifar-10-python.tar.gz'):
-    download_url(train_dataset_url, './data/cifar-10-python.tar.gz', None)
-    download_url(test_dataset_url, './data/cifar-10-python.tar.gz', None)
-
-train_dataset = datasets.CIFAR10(root='./data', train=True, download=False, transform=transforms.ToTensor())
-test_dataset = datasets.CIFAR10(root='./data', train=False, download=False, transform=transforms.ToTensor())
+train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transforms.ToTensor())
+test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transforms.ToTensor())
 
 # Define the data loaders
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=2)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=4, shuffle=False, num_workers=2)
-
-# Define the loss function and optimizer
-# Define the CNN architecture
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x):
-        x = self.pool(torch.relu(self.conv1(x)))
-        x = self.pool(torch.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
 
 # Define the CNN model
 cnn = CNN()
