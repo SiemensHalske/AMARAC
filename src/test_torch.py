8baaa9cffaf7e37c41b66080c7450b06a92cc5e6
SiemensHalske <140380689+SiemensHalske@users.noreply.github.com>
2023-10-31 13:54:42 +0100

    123

--- src/test_torch.py
+++ src/test_torch.py
@@ -160,6 +160,6 @@ model_mobilenet.fit(
 )
 
 # Evaluate the model on the test set
-score = model.evaluate(x_test, y_test, verbose=0)
+score = model_mobilenet.evaluate(x_test, y_test, verbose=0)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
